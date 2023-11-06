981f948ceb960304da1060c9119e594de0d0723c
Andrzej Skuridin <andreyskuridin1995@gmail.com>
2023-11-06 11:13:21 +0300

    chapter 5 completed

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 # Intitialise the app
 app = Flask(__name__)
 
+
 # Define what the app does
 @app.get("/greet")
 def index():
-    """
-    TODO:
-    1. Capture first name & last name
-    2. If either is not provided: respond with an error
-    3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
-    4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
-    5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
-    """
-    return jsonify("TODO")
+    f_name = request.args.get("f_name")
+    lname = request.args.get("lname")
+    if not f_name and not lname:
+        # If both first name and last name are missing, return an error
+        return jsonify({"status": "error"})
+    elif f_name and not lname:
+        # If first name is present but last name is missing
+        response = {"data": f"Hello, {f_name} !"}
+    elif not f_name and lname:
+        # If first name is missing but last name is present
+        response = {"data": f"Hello, Mr. {lname} !"}
+    else:
+        # if none of the above is true, then both names must be present
+        response = {"data": f"Is your name {f_name} {lname} ?"}
+    return render_template('index.html', response=response)
+    #return render_template('index.html', response=jsonify(response))
